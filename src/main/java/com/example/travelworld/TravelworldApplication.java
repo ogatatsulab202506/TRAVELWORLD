bdd0d1da2aee0df7eb477b3fb95423fa03a9fe60
UzuzLab <uzuzlab@gmail.com>
2025-07-14 11:18:28 +0900

    create project

---
+++ src/main/java/com/example/travelworld/TravelworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelworldApplication.class, args);
+	}
+
+}
